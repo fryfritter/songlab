f1ae1b18e5c64a8bb14e4f23554b53879fe4246a
fryfritter <ryan.devqr@gmail.com>
2021-08-24 12:40:47 +0800

    refactor to song router lv 1

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -2,6 +2,10 @@
 const request = require("supertest");
 const app = require("../app");
 
+const songRouter = require("./../routes/songs.route");
+
+app.use("/songs", songRouter);
+
 describe("App", () => {
   it("Testing to see if Jest works", () => {
     expect(1).toBe(1);
@@ -33,20 +37,8 @@ describe("App", () => {
   });
 });
 
-describe("Post : /Songs", () => {
-  it("should return new song object", async () => {
-    const newSong = {
-      name: "yeah song",
-      artist: "lim peh",
-    };
-    const resp = await request(app).post("/songs").send(newSong).expect(201);
-    // expect(resp.body).toEqual(newSong);
-    expect(resp.body).toMatchObject(newSong);
-  });
-});
-
 describe("Get : /Songs", () => {
-  it.only("should return list of songs ", async () => {
+  it("should return list of songs ", async () => {
     const resp = await request(app).get("/songs").expect(200);
 
     // const expectedResult = [
@@ -66,4 +58,55 @@ describe("Get : /Songs", () => {
     expect(responseObject[0]).toMatchObject(expectedResult1);
     expect(responseObject[1]).toMatchObject(expectedResult2);
   });
+
+  it("should return one song", async () => {
+    const resp = await request(app).get("/songs/1").expect(200);
+
+    const expectedResult1 = { name: "someSongName", artist: "someSongArtist" };
+    const responseObject = resp.body;
+    expect(responseObject[0]).toMatchObject(expectedResult1);
+  });
 });
+
+describe("Post : /Songs", () => {
+  it("should return new song object", async () => {
+    const newSong = {
+      name: "yeah song",
+      artist: "lim peh",
+    };
+    const resp = await request(app).post("/songs").send(newSong).expect(201);
+    // expect(resp.body).toEqual(newSong);
+    expect(resp.body).toMatchObject(newSong);
+  });
+});
+
+describe("Put : /Songs", () => {
+  it("should update 2nd song ", async () => {
+    const updatedSongDetail = {
+      name: "updated name",
+      artist: "updated artist",
+    };
+    const resp = await request(app)
+      .put("/songs/2")
+      .send(updatedSongDetail)
+      .expect(200);
+
+    expect(resp.body).toMatchObject(updatedSongDetail);
+  });
+});
+
+describe("Delete :/Songs", () => {
+  it("should remove 2nd song", async () => {
+    const deletedSong = {
+      id: 2,
+      name: "anotherSongName",
+      artist: "anotherArtist",
+    };
+
+    const resp = await request(app).delete("/songs/2").expect(200);
+
+    expect(resp.body).toMatchObject(deletedSong);
+  });
+});
+
+describe.only("Movie List", () => {});
